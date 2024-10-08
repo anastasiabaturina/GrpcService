38269ce1a71e940aac1025c6b10f52eb3d5d50c2
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-10-08 16:22:14 +0300

    test grpc client and server

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7146");
+
+var client = new Greeter.GreeterClient(channel);
+Console.Write("Введите имя: ");
+string? name = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"Ответ сервера: {reply.Message}");
+Console.ReadKey();
